728f81638aad4a00b416496382fe50e361ec1846
makloao <1332553530@qq.com>
2019-06-12 15:44:54 +0800

    SpringMVC2 文件上传

--- src/main/java/com/mkl/Controller/FileUploadController.java
+++ src/main/java/com/mkl/Controller/FileUploadController.java
@@ -1,6 +1,12 @@
 package com.mkl.Controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.multipart.commons.CommonsMultipartFile;
+
+import javax.servlet.http.HttpServletRequest;
+import java.io.*;
 
 @Controller
 public class FileUploadController {
@@ -14,4 +20,22 @@ public class FileUploadController {
  *
  * @create: 2019-06-12 14:39
  **/
+    @RequestMapping("/file")
+    public String file() {
+        return "fileupload";
+    }
+
+    @RequestMapping("/upload")
+    public String fileupload(@RequestParam("file") CommonsMultipartFile file, HttpServletRequest req) throws IOException {
+        String path = req.getRealPath("/WEB-INF");
+        OutputStream os = new FileOutputStream(new File(path, file.getOriginalFilename()));
+        InputStream is = file.getInputStream();
+        int len = 0;
+        byte[] buffer = new byte[400];
+        while((len = is.read(buffer)) != -1)
+            os.write(buffer, 0, len);
+        os.close();
+        is.close();
+        return "index";
+    }
 }
